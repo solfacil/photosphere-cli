378149a3c174f788fc0d9cbdafddb96bad61c0e4
Zoey de Souza Pessanha <zoey.pessanha@solfacil.com.br>
2022-06-08 19:47:01 -0300

    feat: clone command :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use clap::{Args, Parser, Subcommand};
+use std::{io::Error, process::Command};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -7,6 +8,8 @@ use clap::{Args, Parser, Subcommand};
 struct Cli {
     #[clap(subcommand)]
     cmd: Commands,
+    #[clap(long)]
+    ssh: bool,
 }
 
 #[derive(Subcommand)]
@@ -25,19 +28,46 @@ struct Service {
 #[derive(Subcommand)]
 #[clap(arg_required_else_help = true)]
 enum ServiceCommand {
-    New { name: String },
+    New { path: String },
 }
+
+const REPO_NAME: &'static &str = &"service-template";
+const HTTPS_ENDPOINT: &'static &str = &"https://github.com/solfacil/";
+const SSH_ENDPOINT: &'static &str = &"git@github.com:solfacil/";
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
     match &cli.cmd {
-        Commands::Service(service) => {
-            match &service.cmd {
-                ServiceCommand::New { name } => println!("name: {:?}", name),
-            }
-        }
+        Commands::Service(service) => match &service.cmd {
+            ServiceCommand::New { path } => create_service(path, cli.ssh)?,
+        },
     }
-    
+
     Ok(())
+}
+
+fn create_service(path: &str, ssh: bool) -> Result<()> {
+    let repo_url = get_repo_url(ssh);
+    clone_repository(&repo_url, path)?;
+
+    Ok(())
+}
+
+fn clone_repository<'a>(url: &str, dest: &'a str) -> Result<&'a str, Error> {
+    Command::new("git")
+        .arg("clone")
+        .arg(url)
+        .arg(dest)
+        .status()?;
+
+    Ok(dest)
+}
+
+fn get_repo_url(is_ssh: bool) -> String {
+    if is_ssh {
+        return <&str>::clone(SSH_ENDPOINT).to_owned() + REPO_NAME;
+    }
+
+    <&str>::clone(HTTPS_ENDPOINT).to_owned() + REPO_NAME
 }
