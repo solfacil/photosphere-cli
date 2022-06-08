d2db7c621e775ebbfe64dc260ff5e77fec4e053b
Zoey de Souza Pessanha <zoey.pessanha@solfacil.com.br>
2022-06-08 15:37:04 -0300

    feat: basic commands :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Service(Service),
+}
+
+#[derive(Args)]
+#[clap(args_conflicts_with_subcommands = true)]
+struct Service {
+    #[clap(subcommand)]
+    cmd: ServiceCommand,
+}
+
+#[derive(Subcommand)]
+#[clap(arg_required_else_help = true)]
+enum ServiceCommand {
+    New { name: String },
+    Sync,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.cmd {
+        Commands::Service(service) => {
+            match &service.cmd {
+                ServiceCommand::Sync => println!("will sync"),
+                ServiceCommand::New { name } => println!("name: {:?}", name),
+            }
+        }
+    }
+    
+    Ok(())
 }
