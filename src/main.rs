0035c4bac3542fdbaa476b86855c3e652cb1afc1
Zoey de Souza Pessanha <zoey.pessanha@solfacil.com.br>
2022-06-08 21:51:50 -0300

    feat: add project name validation :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use clap::{Args, Parser, Subcommand};
-use std::{io::Error, process::Command};
+use std::{
+    io::Error,
+    path::{Path, PathBuf},
+    process::Command,
+};
+use walkdir::WalkDir;
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
@@ -28,12 +33,18 @@ struct Service {
 #[derive(Subcommand)]
 #[clap(arg_required_else_help = true)]
 enum ServiceCommand {
-    New { path: String },
+    New {
+        #[clap(parse(try_from_str=validate_project_name))]
+        path: String,
+    },
 }
 
 const REPO_NAME: &'static &str = &"service-template";
 const HTTPS_ENDPOINT: &'static &str = &"https://github.com/solfacil/";
 const SSH_ENDPOINT: &'static &str = &"git@github.com:solfacil/";
+const SNAKE_CASE_DEFAULT: &'static &str = &"service_template";
+const CAMMEL_CASE_DEFAULT: &'static &str = &"ServiceTemplate";
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
@@ -54,14 +65,38 @@ fn create_service(path: &str, ssh: bool) -> Result<()> {
     Ok(())
 }
 
-fn clone_repository<'a>(url: &str, dest: &'a str) -> Result<&'a str, Error> {
+fn validate_project_name(name: &str) -> Result<String> {
+    let is_same_default = name.to_lowercase().eq(SNAKE_CASE_DEFAULT);
+    let has_hifen = name.contains('-');
+    let is_valid_name = name.chars().all(is_lower_alphanumeric);
+
+    match (is_same_default, has_hifen, is_valid_name) {
+        (true, _, _) => {
+            bail!(
+                "Hey...that's my name! Please name your project something other than {}.",
+                name
+            )
+        }
+        (_, true, _) => bail!("Please use snake_case for your project name."),
+        (_, _, false) => {
+            bail!("The project name can only contain lower alphanumeric characters and underscore.")
+        }
+        _ => return Ok(name.to_string()),
+    }
+}
+
+fn is_lower_alphanumeric(ch: char) -> bool {
+    (ch.is_alphanumeric() && ch.is_lowercase()) || ch.eq(&'_')
+}
+
+fn clone_repository(url: &str, dest: &str) -> Result<(), Error> {
     Command::new("git")
         .arg("clone")
         .arg(url)
         .arg(dest)
         .status()?;
 
-    Ok(dest)
+    Ok(())
 }
 
 fn get_repo_url(is_ssh: bool) -> String {
